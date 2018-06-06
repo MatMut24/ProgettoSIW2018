64b6d22f8d85eae16f2dc37bc2bb1ef61af95eca
matmut24 <mattiamuti24@gmail.com>
2018-06-06 15:36:32 +0200

    jpa completate

--- ProgettoSIW/src/main/java/it/uniroma3/model/Allievo.java
+++ ProgettoSIW/src/main/java/it/uniroma3/model/Allievo.java
@@ -1,5 +1,118 @@
 package it.uniroma3.model;
 
+import java.sql.Date;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
 public class Allievo {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	
+	@Column(nullable=false)
+	private String nome;
+	
+	@Column(nullable=false)
+	private String cognome;
+	
+	@Column
+	private String email;
+	
+	@Column
+	private String telefono;
+	
+	@Column
+	@Temporal(TemporalType.DATE)
+	private Date dataNascita;
+	
+	@Column
+	private String luogoNascita;
+	
+	@ManyToMany
+	private List<Attivita> attivita;
+
+	public Allievo() {
+		
+	}
+
+	public Allievo(String nome, String cognome, String email, String telefono, Date dataNascita, String luogoNascita) {
+		this.nome = nome;
+		this.cognome = cognome;
+		this.email = email;
+		this.telefono = telefono;
+		this.dataNascita = dataNascita;
+		this.luogoNascita = luogoNascita;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public Date getDataNascita() {
+		return dataNascita;
+	}
+
+	public void setDataNascita(Date dataNascita) {
+		this.dataNascita = dataNascita;
+	}
+
+	public String getLuogoNascita() {
+		return luogoNascita;
+	}
+
+	public void setLuogoNascita(String luogoNascita) {
+		this.luogoNascita = luogoNascita;
+	}
+
+	public List<Attivita> getAttivita() {
+		return attivita;
+	}
+
+	public void setAttivita(List<Attivita> attivita) {
+		this.attivita = attivita;
+	}
+	
+	
 }
--- ProgettoSIW/src/main/java/it/uniroma3/model/Azienda.java
+++ ProgettoSIW/src/main/java/it/uniroma3/model/Azienda.java
@@ -1,5 +1,52 @@
 package it.uniroma3.model;
 
+import java.util.List;
+import java.util.Map;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+
+@Entity
 public class Azienda {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	
+	@OneToMany
+	@JoinColumn(name="azienda_id")
+	private Map<String,Centro> centri;
+	
+	@OneToMany
+	@JoinColumn(name="azienda_id")
+	private Map<String,Allievo> allievi;
+	
+	@OneToMany
+	@JoinColumn(name="azienda_id")
+	private List<Responsabile> responsabili;
+	
+	@OneToMany(mappedBy="azienda")
+	private List<Direttore> direttori;
+
+	public Azienda() {
+
+	}
+
+	public Long getId() {
+		return id;
+	}
+	
+	public Map<String, Centro> getCentri() {
+		return centri;
+	}
+
+	public void setCentri(Map<String, Centro> centri) {
+		this.centri = centri;
+	}
+	
+	
 }
